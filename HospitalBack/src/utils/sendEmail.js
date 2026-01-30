180061ccd6bdebecd64cca6baeda1af748018a96
shaikh junaid <junaidsk4901@gmail.com>
2026-01-30 11:14:38 +0530

    Fix email using Brevo SMTP

--- HospitalBack/src/utils/sendEmail.js
+++ HospitalBack/src/utils/sendEmail.js
@@ -1,26 +1,20 @@
 import nodemailer from "nodemailer";
 
-// Send email using Gmail SMTP (Render compatible)
+// Send email using Brevo SMTP
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: "smtp-relay.brevo.com",
       port: 587,
       secure: false,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER, // apikey
+        pass: process.env.EMAIL_PASS, // SMTP key
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
-      connectionTimeout: 30000,
-      greetingTimeout: 30000,
-      socketTimeout: 30000,
     });
 
     await transporter.sendMail({
-      from: `"Hospitality App" <${process.env.EMAIL_USER}>`,
+      from: `"Hospitality App" <junaidsk4901@gmail.com>`,
       to,
       subject,
       html,
